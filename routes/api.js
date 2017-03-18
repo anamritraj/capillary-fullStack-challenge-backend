141192228d0b5628dfbd4a402563269cee63c3fd
anamritraj <anandamritraj904@gmail.com>
2017-03-18 17:10:21 +0530

    Game API complete

--- routes/api.js
+++ routes/api.js
@@ -4,20 +4,17 @@ var router = express.Router();
 // Include Schema
 var Game = require('../mongodb/game-schema');
 
-console.log(Game);
-
 router.get('/', function (req, res, next) {
     res.send('API for Capillary Hiring challenge.');
 });
 
 
 router.get('/games', function (req, res, next) {
-    console.log(req.body);
-    Game.findOne({}, 'title, platform, score', function (err, games) {
+    Game.find({}, '', function (err, games) {
         if(err) return handleError(err);
-        console.log(games);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(games));
     });
-    res.send('respond with a resource');
 });
 
 module.exports = router;
