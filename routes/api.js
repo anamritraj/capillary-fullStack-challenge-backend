8c1001bfb9eda5224b994258605707b6993f687d
anamritraj <anandamritraj904@gmail.com>
2017-03-18 18:19:24 +0530

    Add middleware for handling headers.

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,15 @@ var router = express.Router();
 // Include Schema
 var Game = require('../mongodb/game-schema');
 
+// Middleware function to handle headers
+router.use(function(req, res, next) {
+    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('access-control-allow-methods', 'GET, POST, PUT');
+    res.header('access-control-allow-origin', '*');
+    res.header('expires', new Date(Date.now()).toUTCString());
+    next();
+});
+
 router.get('/', function (req, res, next) {
     res.send('API for Capillary Hiring challenge.');
 });
